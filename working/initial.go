194c655f7c2185314f905c2a81add605078dbdf1
chancommits <chancommits@gmail.com>
2020-08-09 18:09:04 -0700

    handle view path

--- working/initial.go
+++ working/initial.go
@@ -9,6 +9,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 )
 
@@ -19,21 +20,36 @@ type Page struct {
 	Body  []byte
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-
-	return &Page{Title: title, Body: body}
+	body, err := ioutil.ReadFile(filename)
+	//catch error
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, body}, nil
 }
 
 //he save method returns an error value because that is
 // the return type of WriteFile (a standard library function that writes a byte slice to a file).
+// func (p *Page) save() error {
+// 	filename := p.Title + ".txt"
+// 	return ioutil.WriteFile(filename, p.Body, 0600)
+// }
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
+	//	res,.. error := ioutil.WriteFile()
+
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-
 func main() {
-
+	p1 := &Page{Title: "TestPage", Body: []byte("INSIDE BODY")} //create pagee
+	// save page
+	p1.save()
+	// load page
+	loadPage(p1.Title)
+	// print page body
+	fmt.Println(string(p1.Body))
 }
